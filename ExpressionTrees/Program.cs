45e8bc5e349abd9b2d73c669699776fbf276aedf
Guilherme Marques <guilherme.marques@quartalfs.com>
2019-12-12 16:17:57 +0000

    Include First Example

--- ExpressionTrees/Program.cs
+++ ExpressionTrees/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees
 {
@@ -6,7 +7,20 @@ namespace ExpressionTrees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Expression<Func<int, int, int>> func = (int a, int b) => a + b;
+            int result_1 = func.Compile()(3, 2);
+            Console.WriteLine(result_1);
+
+            ParameterExpression paramA = Expression.Parameter(typeof(int), "a");
+            ParameterExpression paramB = Expression.Parameter(typeof(int), "a");
+            ParameterExpression paramC = Expression.Parameter(typeof(int), "a");
+
+            BinaryExpression sum = Expression.Add(paramA, paramB);
+
+            Expression<Func<int, int, int>> lambda = Expression.Lambda<Func<int, int, int>>(sum, new ParameterExpression[] { paramA, paramB });
+
+            int result_2 = lambda.Compile()(3, 2);
+            Console.WriteLine(result_2);
         }
     }
 }
